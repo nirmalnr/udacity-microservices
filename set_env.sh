9ba41ccbf18f6bf055ae5e0ee2b1bfc0efde9874
Nirmal N R <nirmalmetallica@gmail.com>
2021-03-30 22:42:26 +0530

    Add files via upload

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=postgrespass
+export POSTGRES_HOST=udacity-proj-db.ctjfcw6y3fqd.us-east-2.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=udacityproj-nirmalnr-dev
+export AWS_REGION=us-east-2
+export AWS_PROFILE=default
+export JWT_SECRET=secret
+export URL=http://localhost:8100
